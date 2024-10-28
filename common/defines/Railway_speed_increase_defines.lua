8d44f52b28ccb112158deee562e46f71a3d01d56
shijiudada <59414654+shijiudadayyds@users.noreply.github.com>
2024-10-28 22:01:41 +0800

    Add files via upload

---
+++ common/defines/Railway_speed_increase_defines.lua
@@ -0,0 +1,4 @@
+NDefines.NMilitary.STRATEGIC_SPEED_INFRA_BASE = 8.0  -- 战略转移基础速度 默认5
+NDefines.NMilitary.STRATEGIC_SPEED_INFRA_MAX = 16.0  -- 战略转移最高速度 默认10
+NDefines.NMilitary.STRATEGIC_SPEED_RAIL_BASE = 50.0  -- 战略铁路转移基础速度 默认15
+NDefines.NMilitary.STRATEGIC_SPEED_RAIL_MAX = 100.0  -- 战略铁路转移最高速度 默认25
---
+++ descriptor.mod
@@ -0,0 +1,7 @@
+version="1"
+tags={
+	"Military"
+}
+name="铁路速度提高"
+supported_version="1.14.7"
+remote_file_id="3290990542"
